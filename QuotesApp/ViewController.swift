fda4947c866b4b99cd759bf05e78a0a2ad46501e
Developer-Fha <developer-fha@Developer-Fhas-MacBook-Pro.local>
2017-08-15 14:25:35 +0700

    Initial Commit

---
+++ QuotesApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuotesApp
+//
+//  Created by Developer-Fha on 8/15/2560 BE.
+//  Copyright © 2560 devfha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
